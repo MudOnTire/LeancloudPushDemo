22b7ff0e7c0a9ee148d118521a9d2dfdab5552a0
MudOnTire <895157882@qq.com>
2018-07-12 15:39:14 +0800

    完善处理消息通知逻辑

--- services/PushService.js
+++ services/PushService.js
@@ -1,5 +1,5 @@
 import AV from 'leancloud-storage';
-import { AppState } from 'react-native';
+import { AppState, Alert } from 'react-native';
 const PushNotificationIOS = require('react-native').PushNotificationIOS;
 
 const appId = 'ppdriT1clcnRoda0okCPaB48-gzGzoHsz';
@@ -14,10 +14,30 @@ const Installation = require('leancloud-installation')(AV);
 
 class PushService {
 
+    //用于记录通知的临时变量
+    backgroundNotification = null;
+
     _iOS_initPush = () => {
         PushNotificationIOS.addEventListener('register', this._iOS_onRegister);
         PushNotificationIOS.addEventListener('notification', this._iOS_onNotification);
         PushNotificationIOS.requestPermissions();
+        //监听app状态的改变
+        AppState.addEventListener('change', (newState) => {
+            if (newState === 'active') {
+                if (this.backgroundNotification != null) {
+                    this._iOS_onNotificationTapped();
+                    this.backgroundNotification = null;
+                    this._iOS_cleanBadge();
+                }
+            }
+        });
+        //app关闭时，是否通过点击系统通知打开
+        PushNotificationIOS.getInitialNotification()
+            .then((notification) => {
+                if (notification) {
+                    this._iOS_onNotificationTapped();
+                }
+            });
     }
 
     _iOS_onRegister = (deviceToken) => {
@@ -27,8 +47,12 @@ class PushService {
     }
 
     _iOS_onNotification = (notification) => {
+        //如果app在前台则显示alert
         if (AppState.currentState === 'active') {
             this._showAlert(notification._alert);
+        } else if (AppState.currentState === 'background') {
+            //app在后台运行时点击通知
+            this.backgroundNotification = notification;
         }
     }
 
@@ -44,6 +68,22 @@ class PushService {
             .catch(error => console.error(error))
     }
 
+    _iOS_cleanBadge = () => {
+        Installation.getCurrent()
+            .then((installation) => {
+                installation.set('badge', 0);
+                return installation.save();
+            })
+            .then((result) => {
+                PushNotificationIOS.setApplicationIconBadgeNumber(0);
+            })
+            .catch(error => console.log(error));
+    }
+
+    _iOS_onNotificationTapped = () => {
+        Alert.alert('Notification Tapped');
+    }
+
     _showAlert = (message) => {
         const MessageBarManager = require('react-native-message-bar').MessageBarManager;
         MessageBarManager.showAlert({
@@ -51,11 +91,12 @@ class PushService {
             message: message,
             alertType: 'success',
             stylesheetSuccess: {
-                backgroundColor: '#7851B3', 
-                titleColor: '#fff', 
+                backgroundColor: '#7851B3',
+                titleColor: '#fff',
                 messageColor: '#fff'
             },
-            viewTopInset : 20
+            viewTopInset: 20,
+            onTapped: this._iOS_onNotificationTapped
         });
     }
 }
