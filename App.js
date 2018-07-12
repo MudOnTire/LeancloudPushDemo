72088fa0c234d2d1da78d4d4771f8ba8e94fa23c
MudOnTire <895157882@qq.com>
2018-07-12 10:56:57 +0800

    增加app在前台收到通知并显示的功能

--- App.js
+++ App.js
@@ -9,22 +9,27 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import PushService from './services/PushService';
+const MessageBarAlert = require('react-native-message-bar').MessageBar;
+const MessageBarManager = require('react-native-message-bar').MessageBarManager;
 
 type Props = {};
 export default class App extends Component<Props> {
 
   componentDidMount() {
     PushService._iOS_initPush();
+    MessageBarManager.registerMessageBar(this.refs.alert);
   }
 
   componentWillUnmount() {
     PushNotificationIOS.removeEventListener('register');
+    MessageBarManager.unregisterMessageBar();
   }
 
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text style={{ fontSize: 24 }}>Leancloud Push Demo</Text>
+        <MessageBarAlert ref="alert" />
       </View>
     );
   }
--- services/PushService.js
+++ services/PushService.js
@@ -1,4 +1,5 @@
 import AV from 'leancloud-storage';
+import { AppState } from 'react-native';
 const PushNotificationIOS = require('react-native').PushNotificationIOS;
 
 const appId = 'ppdriT1clcnRoda0okCPaB48-gzGzoHsz';
@@ -15,12 +16,19 @@ class PushService {
 
     _iOS_initPush = () => {
         PushNotificationIOS.addEventListener('register', this._iOS_onRegister);
+        PushNotificationIOS.addEventListener('notification', this._iOS_onNotification);
         PushNotificationIOS.requestPermissions();
     }
 
     _iOS_onRegister = (deviceToken) => {
         if (deviceToken) {
             this._iOS_saveInstallation(deviceToken);
+        }
+    }
+
+    _iOS_onNotification = (notification) => {
+        if (AppState.currentState === 'active') {
+            this._showAlert(notification._alert);
         }
     }
 
@@ -34,6 +42,21 @@ class PushService {
             .then(installation => installation.save(info))
             .then(result => console.log(result))
             .catch(error => console.error(error))
+    }
+
+    _showAlert = (message) => {
+        const MessageBarManager = require('react-native-message-bar').MessageBarManager;
+        MessageBarManager.showAlert({
+            title: '您有一条新的消息',
+            message: message,
+            alertType: 'success',
+            stylesheetSuccess: {
+                backgroundColor: '#7851B3', 
+                titleColor: '#fff', 
+                messageColor: '#fff'
+            },
+            viewTopInset : 20
+        });
     }
 }
 
